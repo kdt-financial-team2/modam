c954cad03e4f2e5b6f325f96f3dca43eb299a466
BH <kbh02180@gamil.com>
2026-05-13 18:35:48 +0900

    에러 수정

--- src/main/java/com/intelliJ_JO/modam/domain/account/entity/Account.java
+++ src/main/java/com/intelliJ_JO/modam/domain/account/entity/Account.java
@@ -11,6 +11,7 @@ import java.time.LocalDateTime;
 @Table(name = "account")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
 public class Account {
 
--- src/main/java/com/intelliJ_JO/modam/domain/card/dto/response/CardResponseDto.java
+++ src/main/java/com/intelliJ_JO/modam/domain/card/dto/response/CardResponseDto.java
@@ -8,17 +8,16 @@ import java.time.LocalDateTime;
 @Getter
 public class CardResponseDto {
     private Long cardId;
-    private String cardNumber;  // 카드 번호
-    private String expiryDate;  // 유효기간 (MM/YY)
-    private String status;      // 상태 (ACTIVE, STOPPED 등)
+    private String cardNumber;
+    private String expiryDate;
+    private String status;
     private LocalDateTime createdAt;
 
-    // Entity를 DTO로 변환하는 생성자 (서비스 로직에서 아주 편하게 쓰입니다!)
     public CardResponseDto(Card card) {
         this.cardId = card.getId();
         this.cardNumber = card.getCardNumber();
         this.expiryDate = card.getExpiryDate();
-        this.status = card.getStatus();
+        this.status = card.getStatus().name();
         this.createdAt = card.getCreatedAt();
     }
 }
