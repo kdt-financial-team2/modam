cbe0932daf537265b429d17ccbb7de44d4cb4d2c
BH <kbh02180@gamil.com>
2026-05-22 14:49:27 +0900

    fix: 빌드 오류 해결

--- src/main/java/com/intelliJ_JO/modam/global/view/AuthViewController.java
+++ src/main/java/com/intelliJ_JO/modam/global/view/AuthViewController.java
@@ -6,9 +6,14 @@ import com.intelliJ_JO.modam.domain.member.service.MemberService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequiredArgsConstructor
 public class AuthViewController {
 
     private final MemberService memberService;
--- src/main/java/com/intelliJ_JO/modam/global/view/MypageViewController.java
+++ src/main/java/com/intelliJ_JO/modam/global/view/MypageViewController.java
@@ -9,11 +9,13 @@ import com.intelliJ_JO.modam.domain.member.entity.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class MypageViewController {
 
     private final AccountMemberRepository accountMemberRepository;
